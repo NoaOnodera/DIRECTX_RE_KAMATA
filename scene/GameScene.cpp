e1e622b7f743b89fb122f87d8ec7166926314bb8
乃碧 小野寺 <deranoir102493@gmail.com>
2022-08-21 16:22:20 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,83 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include "AxisIndicator.h"
+#include "PrimitiveDrawer.h"
+#include <cassert>
+
+
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	delete model_;
+	delete debugCamera_;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+	debugCamera_ = new DebugCamera(1280,720);
+	AxisIndicator::GetInstance()->SetVisible(TRUE);//軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());//軸方向表示が参照するビュープロジェクションを指定する（アドレス渡し）
+
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());//ライン描画が参照するビュープロジェクションを指定する（アドレス渡し）
+	worldTransform_->Initialize();//ワールドトランスフォームの初期化
+	ViewProjection_->Initialize();//ビュープロジェクションの初期化
+}
+
+void GameScene::Update() {
+	debugCamera_->Update();//デバッグカメラの更新
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
